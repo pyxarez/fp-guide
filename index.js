aef094e683f50a4c16bf92441324b05df4ec51ca
pyxarez <pyxarez@gmail.com>
2017-11-26 21:54:41 +0400

    some tasks

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const { curry, prop, compose } = require('lodash/fp');
-const Maybe = require('maybe');
+require('./tasks');
 
-const match = curry((reg, str) => String.prototype.match.call(str, reg));
-const map = curry((f, any_functor) => any_functor.map(f));
-const maybe = curry((x, f, m) => m.isNothing() ? x : f(m.__value));
-
-const add = curry((value, x) => x + value);
-const log = x => console.log(x);
+// const { curry, prop, compose } = require('lodash/fp');
+// const Maybe = require('maybe');
+//
+// const match = curry((reg, str) => String.prototype.match.call(str, reg));
+// const map = curry((f, any_functor) => any_functor.map(f));
+// const maybe = curry((x, f, m) => m.isNothing() ? x : f(m.__value));
+//
+// const add = curry((value, x) => x + value);
+// const log = x => console.log(x);
 
 // const Maybe = x => {
 //   const proto = {
@@ -30,12 +32,12 @@ const log = x => console.log(x);
 //
 // Maybe.of = (x) => Maybe(x);
 
-const a = Maybe(() => Math.random() < 0.5 || undefined);
-
-const range = x => {
-  const a = new Array(x);
-  a.fill(null)
-  return a;
-};
-
-range(20).forEach(() => { log(a.value()); });
+// const a = Maybe(() => Math.random() < 0.5 || undefined);
+//
+// const range = x => {
+//   const a = new Array(x);
+//   a.fill(null)
+//   return a;
+// };
+//
+// range(20).forEach(() => { log(a.value()); });
